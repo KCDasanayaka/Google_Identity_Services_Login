b4f0ad819e40264600616e679a72d8cbb029dc49
KCDasanayaka <116500659+KCDasanayaka@users.noreply.github.com>
2024-04-08 13:58:46 +0530

    ggle

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
 /* global google */
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { jwtDecode } from "jwt-decode";
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState({});
+  
   function handleCallbackResponse(response) {
-    console.log("Encoded JWT ID token: "+ response.credential);  
+    console.log("Encoded JWT ID token: " + response.credential);  
+    var userObject = jwtDecode(response.credential);
+    console.log(userObject);
+    setUser(userObject);
+    document.getElementById("signInDiv").hidden = true;
   }
+
+  function handleSignOut(event){
+    setUser({});
+    document.getElementById("signInDiv").hidden = true;
+  }
+
   useEffect(() => {
     google.accounts.id.initialize({
       client_id: "38761488151-n5hkia5ll5jb5po67odqt2meolk4n5vs.apps.googleusercontent.com",
@@ -19,10 +32,23 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <div id="signInDiv">
-
-      </div>
+    <div>
+      <h2>React Google Login</h2>
+      <br />
+      <br />
+      <div id="signInDiv"></div>
+      {user ? (
+        <div>
+          <h3>User Logged in</h3>
+          <p>Name: {user.name}</p>
+          <p>Email Address: {user.email}</p>
+          <br />
+          <br />
+          <button onClick={handleSignOut}>Log out</button>
+        </div>
+      ) : (
+        <p>Please sign in.</p>
+      )}
     </div>
   );
 }
